058fc5529e482044d8b45bf14f1814bb7ca0decb
MaylethAnto <maylethconforme03@gmail.com>
2024-06-05 17:35:53 -0500

    Agregar archivos de proyecto.

---
+++ Tarea02/Tarea02/View/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea02
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Definimos nuestra pantalla Master
+            Master = new Menu();
+
+            //Definimos nuestra pantalla Detail
+            Detail = new NavigationPage(new Inicio());
+
+            App.Modificador = this;
+        }
+    }
+}
